02d9e270b63e3104e37163c97ef5dabe9588353d
Сёмкин Сергей <semkin@infomatika.ru>
2024-06-17 11:07:20 +0300

    Загрузка проекта

--- packaging.sh
+++ packaging.sh
@@ -2,14 +2,6 @@
 
 venv/bin/pip freeze > requirements.txt
 
-# Проверяем наличие непроиндексированных изменений
-if ! git diff --quiet; then
-    echo "Есть непроиндексированные изменения. Пожалуйста, закоммитьте или отмените их перед продолжением."
-    git status
-    exit 1
-fi
-
-
 # Запрашиваем коммитить изменения
 read -p "Введите текст коммита: " commit_message
 
@@ -18,5 +10,7 @@ echo $commit_message
 git add .
 
 git commit -m "$commit_message"
+
+git push
 
 echo 'Проект собран'
